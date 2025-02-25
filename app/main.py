39ce8c51905eeca25a677b6d2da3893295d99a84
FacelessBob <borislavbebin@gmail.com>
2025-02-25 13:48:27 -0500

    Add invalid command message

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
 
     # Wait for user input
     command = input()
-    print("invalid command: command not found")
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
