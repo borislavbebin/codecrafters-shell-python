4080b5863815da3b8e352b1381cb322780178769
FacelessBob <borislavbebin@gmail.com>
2025-02-25 14:01:50 -0500

    Add echo command

--- app/main.py
+++ app/main.py
@@ -10,6 +10,9 @@ def main():
         command = input()
         if command == "exit 0":
             break
+        commands = command.split(" ")
+        if commands[0] == "echo":
+            print(" ".join(commands[1:]))
         print(f"{command}: command not found")
 
 
